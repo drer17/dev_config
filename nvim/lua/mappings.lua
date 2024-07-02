aa351827b57e5dce98ba76d101249d6d1cffefbe
drer17 <79947155+drer17@users.noreply.github.com>
2024-07-02 21:53:41 +0930

    Update mappings.lua
    added inc rename and refactoring mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -18,4 +18,33 @@ map("n", "<leader>g", function()
   vim.cmd "LazyGit"
 end)
 
+map("n", "<leader>rn", "<cmd> IncRename <CR>")
+
+map("x", "<leader>re", function()
+  require("refactoring").refactor "Extract Function"
+end)
+map("x", "<leader>rf", function()
+  require("refactoring").refactor "Extract Function To File"
+end)
+-- Extract function supports only visual mode
+map("x", "<leader>rv", function()
+  require("refactoring").refactor "Extract Variable"
+end)
+-- Extract variable supports only visual mode
+map("n", "<leader>rI", function()
+  require("refactoring").refactor "Inline Function"
+end)
+-- Inline func supports only normal
+map({ "n", "x" }, "<leader>ri", function()
+  require("refactoring").refactor "Inline Variable"
+end)
+-- Inline var supports both normal and visual mode
+
+map("n", "<leader>rb", function()
+  require("refactoring").refactor "Extract Block"
+end)
+map("n", "<leader>rbf", function()
+  require("refactoring").refactor "Extract Block To File"
+end)
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
